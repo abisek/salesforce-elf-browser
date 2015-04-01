cbe1bf579317ba332ee5b03b28b7b0b263d3b586
Abhishek Sreenivasa <asreenivasa@salesforce.com>
2015-03-31 17:47:05 -0700

    Configure application to run on Puma web server

--- Gemfile
+++ Gemfile
@@ -27,8 +27,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
-# Use Unicorn as the app server
-# gem 'unicorn'
+# Use Puma as the app server
+gem 'puma'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
---
+++ config/puma.rb
@@ -0,0 +1,16 @@
+workers Integer(ENV['WEB_CONCURRENCY'] || 3)
+threads_count = Integer(ENV['MAX_THREADS'] || 2)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  # ActiveRecord is not used in this project. Uncomment if necessary.
+  # ActiveRecord::Base.establish_connection
+end
