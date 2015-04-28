987038398e0d905dd7e41463e3f2c22b6dbbde75
Abhishek Sreenivasa <asreenivasa@salesforce.com>
2015-04-27 22:00:15 -0700

    Configurable maximum file size limiter

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,9 @@ Bundler.require(*Rails.groups)
 module ElfBrowser
   class Application < Rails::Application
     config.middleware.use Rack::Deflater
+
+    # Maximum file size that can be downloaded via event log files
+    config.x.elf.max_download_file_size_in_bytes = Float(ENV['ELF_MAX_DOWNLOAD_FILE_SIZE_IN_BYTES'] || 5_000_000)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
